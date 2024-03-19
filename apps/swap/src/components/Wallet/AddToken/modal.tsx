ea063b669490c1d4f08c65f7c47ba31c7db30c8a
honopu <thebestisaskingforafriend@proton.me>
2024-03-19 13:36:23 +0800

    feat(swap): auto add to wallet if import token successfully

--- apps/swap/src/components/Wallet/AddToken/modal.tsx
+++ apps/swap/src/components/Wallet/AddToken/modal.tsx
@@ -256,7 +256,13 @@ export default function AddTokenModal({ open, onClose }: { open: boolean; onClos
         </Grid>
       </Modal>
 
-      {importTokenShow ? <ImportToken open={importTokenShow} onClose={() => setImportTokenShow(false)} /> : null}
+      {importTokenShow ? (
+        <ImportToken
+          open={importTokenShow}
+          onClose={() => setImportTokenShow(false)}
+          onImportSuccessfully={() => onClose()}
+        />
+      ) : null}
     </>
   );
 }
