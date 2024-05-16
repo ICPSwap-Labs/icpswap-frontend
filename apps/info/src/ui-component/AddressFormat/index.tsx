03f80dd2f5fc729e065ba18a3d2eb07d88373e2e
honopu <85238703+honopu@users.noreply.github.com>
2024-05-16 16:18:08 +0800

    refactor(info): enhance Copy component

--- apps/info/src/ui-component/AddressFormat/index.tsx
+++ apps/info/src/ui-component/AddressFormat/index.tsx
@@ -1,6 +1,38 @@
-import Copy from "ui-component/copy/copy";
+import Copy, { CopyRef } from "ui-component/copy/copy";
 import { shorten } from "@icpswap/utils";
+import { Typography, type TypographyProps } from "@mui/material";
+import { useRef } from "react";
 
-export default function AddressFormat({ address }: { address: string }) {
-  return <Copy content={address}>{shorten(address, 8)}</Copy>;
+export interface AddressFormatProps {
+  sx?: TypographyProps["sx"];
+  address: string;
+  length?: number;
+}
+
+export default function AddressFormat({ address, sx, length = 8 }: AddressFormatProps) {
+  const copyRef = useRef<CopyRef>(null);
+
+  const handleCopy = () => {
+    if (copyRef) {
+      copyRef.current?.copy();
+    }
+  };
+
+  return (
+    <>
+      <Copy content={address} hide ref={copyRef}>
+        {shorten(address, 8)}
+      </Copy>
+      <Typography
+        color="text.primary"
+        sx={{
+          cursor: "pointer",
+          ...sx,
+        }}
+        onClick={handleCopy}
+      >
+        {shorten(address, length)}
+      </Typography>
+    </>
+  );
 }
