49db5200960d9560719f7763a077953d94df56c0
honopu <85238703+honopu@users.noreply.github.com>
2024-07-25 14:25:10 +0800

    feat(nns): add/delete followee in batchs

--- apps/swap/src/views/sns/Neurons/components/DeleteFollowee.tsx
+++ apps/swap/src/views/sns/Neurons/components/DeleteFollowee.tsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import { toHexString } from "@icpswap/utils";
-import { setNeuronFollows } from "@icpswap/hooks";
-import { useTips, TIP_ERROR, TIP_SUCCESS, useFullscreenLoading } from "hooks/useTips";
-import { t } from "@lingui/macro";
 import { Neuron } from "@icpswap/types";
 import { X } from "react-feather";
-import { ConfirmModal } from "@icpswap/ui";
+
+import { DeleteFolloweeModal } from "./DeleteFolloweeModal";
 
 export interface DeleteFolloweeProps {
   onDeleteSuccess?: () => void;
@@ -25,61 +22,20 @@ export function DeleteFollowee({
   follow_id,
 }: DeleteFolloweeProps) {
   const [open, setOpen] = useState(false);
-  const [openFullscreenLoading, closeFullscreenLoading] = useFullscreenLoading();
-  const [openTip] = useTips();
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const handleDeleteFollowee = async () => {
-    if (loading || !governance_id || !neuron || !neuron_id) return;
-
-    setOpen(false);
-    setLoading(true);
-    openFullscreenLoading();
-
-    const follow_result = neuron.followees.find(([id]) => id === func_id);
-
-    if (!follow_result) {
-      openTip(t`Some unknown error happened`, TIP_ERROR);
-      setLoading(false);
-      closeFullscreenLoading();
-      return;
-    }
-
-    const [id, { followees }] = follow_result;
-
-    const new_followees = followees.filter((followee) => toHexString(followee.id) !== toHexString(follow_id));
-
-    const { data, message, status } = await setNeuronFollows(governance_id, neuron_id, id, new_followees);
-
-    const result = data ? data.command[0] : undefined;
-    const split_neuron_error = result ? ("Error" in result ? result.Error : undefined) : undefined;
-
-    if (status === "ok") {
-      if (!split_neuron_error) {
-        openTip(t`Delete followees successfully`, TIP_SUCCESS);
-        if (onDeleteSuccess) onDeleteSuccess();
-      } else {
-        const message = split_neuron_error.error_message;
-        openTip(message !== "" ? message : t`Failed to delete followees`, TIP_ERROR);
-      }
-    } else {
-      openTip(message ?? t`Failed to delete followees`, TIP_ERROR);
-    }
-
-    setLoading(false);
-    closeFullscreenLoading();
-  };
 
   return (
     <>
       <X style={{ cursor: "pointer" }} size="18px" onClick={() => setOpen(true)} />
 
-      <ConfirmModal
-        open={open}
-        onConfirm={handleDeleteFollowee}
+      <DeleteFolloweeModal
         onClose={() => setOpen(false)}
-        title={t`Delete Followee`}
-        text={t`Are you sure to delete this followee?`}
+        open={open}
+        onDeleteSuccess={onDeleteSuccess}
+        follow_ids={[follow_id]}
+        neuron_id={neuron_id}
+        governance_id={governance_id}
+        neuron={neuron}
+        func_ids={[func_id]}
       />
     </>
   );
