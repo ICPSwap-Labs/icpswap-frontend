47cf86ccf9e75e115639b15025ad8d3ebd032f5c
honopu <85238703+honopu@users.noreply.github.com>
2024-09-15 14:51:14 +0800

    refactor: enhancement

--- apps/info/src/views/swap-scan/SwapTransactions.tsx
+++ apps/info/src/views/swap-scan/SwapTransactions.tsx
@@ -163,12 +163,12 @@ function Transactions({ address }: TransactionsProps) {
                 <BodyCell>{formatDollarAmount(transaction.amountUSD, 3)}</BodyCell>
 
                 <BodyCell>
-                  {formatAmount(transaction.token0ChangeAmount, 6)}{" "}
+                  {formatAmount(transaction.token0ChangeAmount, 4)}{" "}
                   <SwapTransactionPriceTip symbol={transaction.token0Symbol} price={transaction.token0Price} />
                 </BodyCell>
 
                 <BodyCell>
-                  {formatAmount(transaction.token1ChangeAmount, 6)}{" "}
+                  {formatAmount(transaction.token1ChangeAmount, 4)}{" "}
                   <SwapTransactionPriceTip symbol={transaction.token1Symbol} price={transaction.token1Price} />
                 </BodyCell>
 
