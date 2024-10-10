77004dfd9c0996dd9000ce11164a4287cb190df8
honopu <85238703+honopu@users.noreply.github.com>
2024-10-10 11:19:01 +0800

    Enhancement

--- apps/swap/src/views/swap-pro/TokenChart/Charts.tsx
+++ apps/swap/src/views/swap-pro/TokenChart/Charts.tsx
@@ -20,13 +20,13 @@ export default function TokenChartInfo() {
   return (
     <Box
       sx={{
-        margin: "22px 0 0 0",
+        margin: "10px 0 0 0",
         background: theme.palette.background.level3,
         borderBottomLeftRadius: "12px",
         borderBottomRightRadius: "12px",
         overflow: "hidden",
         "@media(max-width: 640px)": {
-          margin: "16px 0 0 0",
+          margin: "0 0 0 0",
         },
       }}
     >
