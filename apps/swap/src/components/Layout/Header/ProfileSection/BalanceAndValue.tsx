6821cc83d675657f15dcc2f6d068b9b4f9fe09f5
honopu <85238703+honopu@users.noreply.github.com>
2024-11-16 22:02:42 +0800

    refactor(swap): only show icp balance in profile

--- apps/swap/src/components/Layout/Header/ProfileSection/BalanceAndValue.tsx
+++ apps/swap/src/components/Layout/Header/ProfileSection/BalanceAndValue.tsx
@@ -1,22 +1,16 @@
-import { useMemo } from "react";
 import { Typography } from "components/Mui";
-import { BigNumber, formatDollarAmountV1, nonNullArgs, toSignificantWithGroupSeparator } from "@icpswap/utils";
+import { formatDollarAmountV1, parseTokenAmount, toSignificantWithGroupSeparator } from "@icpswap/utils";
 import { ICP } from "@icpswap/tokens";
-import { useHelperUserTokens } from "@icpswap/hooks";
 import { Flex } from "@icpswap/ui";
 import { useAccountPrincipal } from "store/auth/hooks";
 import { useInfoToken } from "hooks/info/useInfoTokens";
+import { useTokenBalance } from "hooks/token/useTokenBalance";
 
 export function BalanceAndValue() {
   const principal = useAccountPrincipal();
 
   const infoToken = useInfoToken(ICP.address);
-  const { result: userTokens, loading } = useHelperUserTokens({ principal: principal?.toString() });
-
-  const totalBalance = useMemo(() => {
-    if (loading) return undefined;
-    return userTokens?.totalBalance ?? 0;
-  }, [userTokens, loading]);
+  const { result: tokenBalance } = useTokenBalance(ICP.address, principal);
 
   return (
     <Flex sx={{ padding: "20px 0 0 0" }} justify="center" vertical align="center">
@@ -25,12 +19,17 @@ export function BalanceAndValue() {
                       </Typography> */}
 
       <Typography sx={{ fontSize: "28px", fontWeight: 500, margin: "12px 0 0 0", color: "text.primary" }}>
-        {nonNullArgs(totalBalance) ? formatDollarAmountV1({ num: totalBalance, ab: 0.01 }) : "--"}
+        {tokenBalance && infoToken
+          ? formatDollarAmountV1({
+              num: parseTokenAmount(tokenBalance, ICP.decimals).multipliedBy(infoToken.priceUSD).toString(),
+              ab: 0.01,
+            })
+          : "--"}
       </Typography>
 
       <Typography sx={{ fontSize: "16px", margin: "8px 0 0 0" }}>
-        {nonNullArgs(totalBalance) && infoToken
-          ? toSignificantWithGroupSeparator(new BigNumber(totalBalance).dividedBy(infoToken.priceUSD).toString(), 4)
+        {tokenBalance
+          ? toSignificantWithGroupSeparator(parseTokenAmount(tokenBalance, ICP.decimals).toString(), 4)
           : "--"}{" "}
         ICP
       </Typography>
