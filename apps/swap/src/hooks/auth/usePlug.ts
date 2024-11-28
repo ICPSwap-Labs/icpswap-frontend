a5ac870ab35a1054bea31aaf1375a5b2b724eb34
honopu <85238703+honopu@users.noreply.github.com>
2024-11-28 12:03:38 +0800

    Fix

--- apps/swap/src/hooks/auth/usePlug.ts
+++ apps/swap/src/hooks/auth/usePlug.ts
@@ -5,7 +5,7 @@ export function usePlugExternalDisconnect() {
   const logout = useUserLogout();
 
   useEffect(() => {
-    if (window.ic.plug) {
+    if (window.ic && window.ic.plug) {
       if (window.ic.plug.onExternalDisconnect) window.ic.plug.onExternalDisconnect(logout);
       if (window.ic.plug.onLockStateChange)
         window.ic.plug.onLockStateChange((isLocked) => {
