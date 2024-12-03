6ac2f2b7627c89305d6a46af8035edb7ffd8c834
honopu <85238703+honopu@users.noreply.github.com>
2024-12-03 17:35:07 +0800

    Fix: plug loop disconnect

--- apps/swap/src/hooks/auth/usePlug.ts
+++ apps/swap/src/hooks/auth/usePlug.ts
@@ -1,16 +1,22 @@
 import { useEffect } from "react";
-import { useUserLogout } from "store/auth/hooks";
+import { useCleanLogState } from "store/auth/hooks";
 
 export function usePlugExternalDisconnect() {
-  const logout = useUserLogout();
+  const cleanLogState = useCleanLogState();
 
   useEffect(() => {
     if (window.ic && window.ic.plug) {
-      if (window.ic.plug.onExternalDisconnect) window.ic.plug.onExternalDisconnect(logout);
+      if (window.ic.plug.onExternalDisconnect) {
+        window.ic.plug.onExternalDisconnect(() => {
+          // Do not use useUserLogout, logout will exec window.ic.plug.onExternalDisconnect, and case a Loop execution
+          cleanLogState();
+        });
+      }
+
       if (window.ic.plug.onLockStateChange)
         window.ic.plug.onLockStateChange((isLocked) => {
           if (isLocked) {
-            logout();
+            cleanLogState();
           }
         });
     }
