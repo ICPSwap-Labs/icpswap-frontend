44467bcf8b43e648494f1a897244d65d95f339b6
honopu <85238703+honopu@users.noreply.github.com>
2025-01-23 11:40:25 +0800

    Add principalToSubaccount

--- packages/utils/src/principal.ts
+++ packages/utils/src/principal.ts
@@ -1,19 +1,13 @@
 import { AccountIdentifier, SubAccount } from "@dfinity/ledger-icp";
 import { Principal } from "@dfinity/principal";
+
 import { isSubAccount } from "./ic";
 import { isValidPrincipal } from "./isValidPrincipal";
 
-export function principalToAccount(
-  principal: string,
-  subAccount?: SubAccount | string
-): string {
+export function principalToAccount(principal: string, subAccount?: SubAccount | string): string {
   if (!principal) return principal;
 
-  if (
-    subAccount &&
-    !isSubAccount(subAccount) &&
-    !isValidPrincipal(subAccount)
-  ) {
+  if (subAccount && !isSubAccount(subAccount) && !isValidPrincipal(subAccount)) {
     throw Error("Wrong sub account");
   }
 
@@ -29,4 +23,8 @@ export function principalToAccount(
     principal: Principal.fromText(principal),
     subAccount: sub,
   }).toHex();
+}
+
+export function principalToSubaccount(principal: string): Uint8Array {
+  return SubAccount.fromPrincipal(Principal.fromText(principal)).toUint8Array();
 }
