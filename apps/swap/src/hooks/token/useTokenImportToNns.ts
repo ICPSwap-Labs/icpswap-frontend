77e3616687ba18bf9ebd64a393d1c6824bcecb0d
honopu <85238703+honopu@users.noreply.github.com>
2025-02-05 14:15:19 +0800

    fix: importing of chain key tokens

--- apps/swap/src/hooks/token/useTokenImportToNns.ts
+++ apps/swap/src/hooks/token/useTokenImportToNns.ts
@@ -1,25 +1,20 @@
 import { useMemo } from "react";
 import type { Null } from "@icpswap/types";
 import { isNullArgs } from "@icpswap/utils";
-import { ICP } from "@icpswap/tokens";
+import { ckBTC, ckETH, ckUSDC, ICP } from "@icpswap/tokens";
 import { useTokenStandard } from "store/token/cache/hooks";
-import { useBridgeTokens } from "store/global/hooks";
 
 import { useIsSnsToken } from "./useTokenInSNS";
+
+const ImportedTokens = [ICP.address, ckBTC.address, ckUSDC.address, ckETH.address];
 
 export function useTokenImportToNns(tokenId: string | Null) {
   const isSnsToken = useIsSnsToken(tokenId);
   const tokenStandard = useTokenStandard(tokenId);
-  const bridgeTokens = useBridgeTokens();
 
   return useMemo(() => {
     if (isNullArgs(isSnsToken) || isNullArgs(tokenStandard) || isNullArgs(tokenId)) return false;
 
-    return (
-      isSnsToken === false &&
-      tokenStandard.includes("ICRC") &&
-      tokenId !== ICP.address &&
-      !bridgeTokens.includes(tokenId)
-    );
-  }, [tokenId, isSnsToken, tokenStandard, bridgeTokens]);
+    return isSnsToken === false && tokenStandard.includes("ICRC") && !ImportedTokens.includes(tokenId);
+  }, [tokenId, isSnsToken, tokenStandard]);
 }
