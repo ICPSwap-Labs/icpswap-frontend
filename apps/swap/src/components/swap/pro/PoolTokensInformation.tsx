fe0301a140eec49a820fbf48037634e6db1c594e
honopu <85238703+honopu@users.noreply.github.com>
2025-04-19 19:14:54 +0800

    refactor(swap-pro): only show one token Information

--- apps/swap/src/components/swap/pro/PoolTokensInformation.tsx
+++ apps/swap/src/components/swap/pro/PoolTokensInformation.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Box, Typography, useTheme } from "components/Mui";
+import { Box } from "components/Mui";
 import { LoadingRow } from "@icpswap/ui";
 import type { Null } from "@icpswap/types";
 import { Token } from "@icpswap/swap-sdk";
@@ -9,7 +9,6 @@ import { SwapProCardWrapper } from "./SwapProWrapper";
 import { TokenInformation } from "./TokenInformation";
 
 export function PoolTokensInformation() {
-  const theme = useTheme();
   const { inputToken, outputToken, poolId } = useContext(SwapContext);
 
   const [token, setToken] = useState<Token | Null>(null);
@@ -33,38 +32,6 @@ export function PoolTokensInformation() {
     <SwapProCardWrapper padding={token0 && token1 && token ? "0px" : "16px"}>
       {token0 && token1 && token ? (
         <Box sx={{ padding: "16px 0 0 0" }}>
-          <Box sx={{ padding: "0 16px" }}>
-            <Box
-              sx={{
-                width: "100%",
-                borderRadius: "8px",
-                background: theme.palette.background.level2,
-                border: `1px solid ${theme.palette.background.level4}`,
-                padding: "2px",
-                display: "flex",
-              }}
-            >
-              {[token0, token1].map((element) => (
-                <Box
-                  key={element.address}
-                  sx={{
-                    flex: "50%",
-                    background: token.address === element.address ? theme.palette.background.level1 : "none",
-                    borderRadius: "6px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "24px",
-                    cursor: "pointer",
-                  }}
-                  onClick={() => setToken(element)}
-                >
-                  <Typography color="text.primary">{element.symbol}</Typography>
-                </Box>
-              ))}
-            </Box>
-          </Box>
-
           <TokenInformation token={token} poolId={poolId} />
         </Box>
       ) : (
