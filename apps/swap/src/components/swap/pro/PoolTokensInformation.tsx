cf8ed79a6148213f0d579d4ca0ba33682e9f806d
honopu <85238703+honopu@users.noreply.github.com>
2025-04-25 15:31:41 +0800

    fix(swap-pro): pool tokens information

--- apps/swap/src/components/swap/pro/PoolTokensInformation.tsx
+++ apps/swap/src/components/swap/pro/PoolTokensInformation.tsx
@@ -1,37 +1,68 @@
-import { useContext, useEffect, useState } from "react";
-import { Box } from "components/Mui";
+import { useContext, useMemo, useState } from "react";
+import { Box, Typography, useTheme } from "components/Mui";
 import { LoadingRow } from "@icpswap/ui";
-import type { Null } from "@icpswap/types";
-import { Token } from "@icpswap/swap-sdk";
 import { SwapContext } from "components/swap/index";
-
+import { ICP } from "@icpswap/tokens";
+import { isNullArgs } from "@icpswap/utils";
 import { SwapProCardWrapper } from "./SwapProWrapper";
 import { TokenInformation } from "./TokenInformation";
 
 export function PoolTokensInformation() {
+  const theme = useTheme();
   const { inputToken, outputToken, poolId } = useContext(SwapContext);
 
-  const [token, setToken] = useState<Token | Null>(null);
-  const [token0, setToken0] = useState<Token | Null>(null);
-  const [token1, setToken1] = useState<Token | Null>(null);
+  const [inverted, setInverted] = useState(false);
 
-  useEffect(() => {
-    if (inputToken && outputToken) {
-      const sorted = inputToken.sortsBefore(outputToken);
-
-      const token0 = sorted ? inputToken : outputToken;
-      const token1 = sorted ? outputToken : inputToken;
-
-      setToken(token0);
-      setToken0(token0);
-      setToken1(token1);
-    }
+  const tokensShowInformation = useMemo(() => {
+    if (!inputToken || !outputToken) return [];
+    const tokens = [inputToken, outputToken].filter((token) => token.address !== ICP.address);
+    return tokens;
   }, [inputToken, outputToken]);
 
+  const token = useMemo(() => {
+    if (isNullArgs(inputToken) || isNullArgs(outputToken)) return undefined;
+    if (tokensShowInformation.length === 1) return tokensShowInformation[0];
+    return inverted ? inputToken : outputToken;
+  }, [inputToken, outputToken, inverted, tokensShowInformation]);
+
   return (
-    <SwapProCardWrapper padding={token0 && token1 && token ? "0px" : "16px"}>
-      {token0 && token1 && token ? (
+    <SwapProCardWrapper padding={inputToken && outputToken && token ? "0px" : "16px"}>
+      {token ? (
         <Box sx={{ padding: "16px 0 0 0" }}>
+          {tokensShowInformation.length === 2 ? (
+            <Box sx={{ padding: "0 16px" }}>
+              <Box
+                sx={{
+                  width: "100%",
+                  borderRadius: "8px",
+                  background: theme.palette.background.level2,
+                  border: `1px solid ${theme.palette.background.level4}`,
+                  padding: "2px",
+                  display: "flex",
+                }}
+              >
+                {tokensShowInformation.map((element) => (
+                  <Box
+                    key={element.address}
+                    sx={{
+                      flex: "50%",
+                      background: token.address === element.address ? theme.palette.background.level1 : "none",
+                      borderRadius: "6px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: "24px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setInverted(!inverted)}
+                  >
+                    <Typography color="text.primary">{element.symbol}</Typography>
+                  </Box>
+                ))}
+              </Box>
+            </Box>
+          ) : null}
+
           <TokenInformation token={token} poolId={poolId} />
         </Box>
       ) : (
