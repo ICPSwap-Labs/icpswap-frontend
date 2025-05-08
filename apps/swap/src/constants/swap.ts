2e2226c5a7f6da998033034328375d0f054623c8
honopu <85238703+honopu@users.noreply.github.com>
2025-05-08 21:51:43 +0800

    feats: add swap pro entry in limit simple mode

--- apps/swap/src/constants/swap.ts
+++ apps/swap/src/constants/swap.ts
@@ -27,4 +27,9 @@ export const icrc_standards = [TOKEN_STANDARD.ICRC1, TOKEN_STANDARD.ICRC2];
 
 export const TRADING_VIEW_DESCRIPTIONS = [ChartView.PRICE, ChartView.DexTools, ChartView.DexScreener];
 
+export enum Tab {
+  Swap = "Swap",
+  Limit = "Limit",
+}
+
 export * from "./mint";
