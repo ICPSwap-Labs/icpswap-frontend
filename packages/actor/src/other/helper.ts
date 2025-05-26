062bf3501f8d813fbc1e8b9cb707ab67e43e1bee
honopu <85238703+honopu@users.noreply.github.com>
2025-05-26 11:00:12 +0800

    refactor: change the api of sync-user's-token

--- packages/actor/src/other/helper.ts
+++
@@ -1,8 +0,0 @@
-import { ToolService, ToolInterfaceFactory } from "@icpswap/candid";
-import { actor } from "../actor";
-
-export const userTokenHelper = () =>
-  actor.create<ToolService>({
-    canisterId: "mecty-aaaaa-aaaag-qkp6a-cai",
-    idlFactory: ToolInterfaceFactory,
-  });
