75bd943a30e86a93cc11701a011a6ceabffc9a76
honopu <85238703+honopu@users.noreply.github.com>
2025-06-12 11:25:35 +0800

    feats fetch with get method

--- packages/utils/src/fetch_post.ts
+++ packages/utils/src/fetch_post.ts
@@ -2,6 +2,7 @@ import { IcpSwapAPIResult } from "@icpswap/types";
 import { ICPSWAP_API } from "@icpswap/constants";
 
 import { resultFormat } from "./resultFormat";
+import { nonNullArgs } from "./isNullArgs";
 
 export async function fetch_post<T>(api: string, data?: any) {
   const fetch_result = await fetch(api, {
@@ -22,6 +23,42 @@ export async function fetch_post<T>(api: string, data?: any) {
   return undefined;
 }
 
+export async function fetch_get<T>(api: string, data?: any) {
+  const __data = {};
+
+  if (data) {
+    Object.keys(data).forEach((key) => {
+      if (nonNullArgs(data[key])) {
+        __data[key] = data[key];
+      }
+    });
+  }
+
+  const fetch_result = await fetch(`${api}${data ? `?${new URLSearchParams(__data).toString()}` : ""}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  }).catch(() => undefined);
+
+  if (!fetch_result) return undefined;
+
+  const result = (await fetch_result.json()) as IcpSwapAPIResult<T> | undefined;
+
+  if (result.code === 200) return resultFormat<T>(result.data);
+
+  return undefined;
+}
+
 export async function icpswap_fetch_post<T>(api: string, data?: any) {
   return fetch_post<T>(`${ICPSWAP_API}${api}`, data);
+}
+
+export async function icpswap_info_fetch_post<T>(api: string, data?: any) {
+  return fetch_post<T>(`${ICPSWAP_API}/info${api}`, data);
+}
+
+export async function icpswap_info_fetch_get<T>(api: string, data?: any) {
+  return fetch_get<T>(`${ICPSWAP_API}/info${api}`, data);
 }
