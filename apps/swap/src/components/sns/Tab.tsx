c95756580cf7136cd58dfc8fee51939f4a43b7f3
honopu <85238703+honopu@users.noreply.github.com>
2025-06-18 10:15:16 +0800

    fix: the root id is undefined

--- apps/swap/src/components/sns/Tab.tsx
+++ apps/swap/src/components/sns/Tab.tsx
@@ -1,14 +1,17 @@
 import { useParsedQueryString } from "@icpswap/hooks";
 import { TabPanel } from "components/index";
+import { DEFAULT_ROOT_ID } from "constants/nns";
 import { useMemo } from "react";
 
 export function Tabs() {
   const { root_id } = useParsedQueryString() as { root_id: string };
 
   const tabs = useMemo(() => {
+    const __root_id = root_id ?? DEFAULT_ROOT_ID;
+
     return [
-      { key: "neurons", value: "Neurons", path: `/sns/neurons?root_id=${root_id}` },
-      { key: "voting", value: "Voting", path: `/sns/voting?root_id=${root_id}` },
+      { key: "neurons", value: "Neurons", path: `/sns/neurons?root_id=${__root_id}` },
+      { key: "voting", value: "Voting", path: `/sns/voting?root_id=${__root_id}` },
       { key: "launches", value: "Launchpad", path: "/sns/launches" },
     ];
   }, [root_id]);
