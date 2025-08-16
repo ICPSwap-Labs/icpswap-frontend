51f57163c97f48dade7999de144235fe60545249
honopu <85238703+honopu@users.noreply.github.com>
2025-08-16 09:06:22 +0800

    feat(ck-bridge): change bitcoin confirmations to 6

--- apps/swap/src/constants/ckBTC.ts
+++ apps/swap/src/constants/ckBTC.ts
@@ -10,7 +10,7 @@ export const BTC_DISSOLVE_REFRESH = "BTC_DISSOLVE_REFRESH";
 
 export const BTC_MINT_REFRESH = "BTC_MINT_REFRESH";
 
-export const BITCOIN_CONFIRMATIONS = 12;
+export const BITCOIN_CONFIRMATIONS = 6;
 
 export const bitcoinBlockExplorer = (height: number) => `https://www.blockchain.com/explorer/blocks/btc/${height}`;
 
