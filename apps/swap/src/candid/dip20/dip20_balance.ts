cf582364756783fc32100dac0ae62462e16a55bc
honopu <85238703+honopu@users.noreply.github.com>
2025-12-16 20:46:26 +0800

    refactor: remove unused candid

--- apps/swap/src/candid/dip20/dip20_balance.ts
+++
@@ -1,5 +0,0 @@
-import type { Principal } from "@dfinity/principal";
-
-export default interface _SERVICE {
-  balanceOf: (arg_0: Principal) => Promise<bigint>;
-}
--- apps/swap/src/candid/dip20/dip20_totalSupply.did.ts
+++
@@ -1,5 +0,0 @@
-export default ({ IDL }: any) => {
-  return IDL.Service({
-    totalSupply: IDL.Func([], [IDL.Nat], []),
-  });
-};
